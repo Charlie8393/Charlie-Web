bacffca81446aa5c191f3fd803ca86a6ea661e1f
손동찬 <charliesohn@sondongchan-ui-MacBookAir.local>
2021-11-12 09:13:12 +0900

    submitting

---
+++ Homework/Homewokr.js
@@ -0,0 +1,13 @@
+//It will show the Kelvin.
+const Kelvin = 293;
+
+//It will show the Celsius.
+let Celsius = Kelvin - 273;
+
+//It will show the Fahrenheit.
+let Fahrenheit = (Celsius * (9/5)) + 32
+
+// It will convert from Celsius to Fahrenheit
+let math = Math.floor(Fahrenheit);
+
+console.log("The temperature is " + math + " degrees Fahrenheit."); //It will print The temperature is 68 degrees Fahrenheit.
